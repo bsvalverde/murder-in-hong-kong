d361517810ca9d1672d1da417c3f6c6084193a5f
bsvalverde <bsvalverde@gmail.com>
2021-03-20 23:11:42 -0300

    Throws error if already logged in

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,12 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { login, logout } from '../express/authentication';
+import { CodedError } from '../models/errors';
 
 export default class AuthController {
   async login(req: Request, res: Response, next: NextFunction): Promise<void> {
     if (req.user) {
-      // throw error
-      res.json(req.user);
-      return;
+      throw new CodedError('Already logged in', 409);
     }
     try {
       const user = await login(req);
